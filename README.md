73e77d8a7b22ee365ac9eeb652fc46d77d0d2bb8
Ganesh K <ganeshkannan627@gmail.com>
2024-03-16 22:35:59 +0530

    updating day 1

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# appium-workshop
+# appium-workshop-16march2024
 Jumpstart your mobile testing journey with appium
+
+# 00. Pre-requisites
+1. [Java JDK](https://www.java.com/en/)
+2. [NodeJS LTS](https://nodejs.org/en/download/current)
+3. [Appium 2.5](https://appium.io/docs/en/2.5/quickstart/install/)
+4. [Appium drivers](https://appium.io/docs/en/2.5/ecosystem/drivers/)
+5. [Flutter driver](https://github.com/appium/appium-flutter-driver) - if needed
+6. [Appium Inspector](https://github.com/appium/appium-inspector/releases)
+7. [Appium Doctor](#) -Health check
+8. [Android Studio](https://developer.android.com/studio)
+9. [Android Virtual Device](https://developer.android.com/studio/run/managing-avds) or Real Device or Cloud Device
+10. [Scrcpy](https://github.com/Genymobile/scrcpy) -Screensharing library for real device
+
+# 01. Setup and start AVD or Real Device
+
+```bash
+adb devices
+```
+
+# 02. Start Appium Server
+
+# 03. Connect Appium Inspector through JSON Capabilities
+
+# 04. Finding Activity to Launch Already Installed Application
+
+[testsigma Reference](https://support.testsigma.com/support/solutions/articles/32000019977-how-to-find-app-package-and-app-activity-of-your-android-app)
+
+
+## To ensure device/emulator running
+
+```bash
+adb devices
+```
+
+## Getting Activity and Package name
+### For Mac/Linux:
+```bash
+adb shell dumpsys window | grep -E 'mCurrentFocus'
+```
+### For Windows:
+
+```bash
+adb shell dumpsys window | find "mCurrentFocus
+```
+
+### Output
+
+```bash
+  mCurrentFocus=Window{c5d6e72 u0 com.android.calculator2/com.android.calculator2.Calculator}
+```
+Package Name: *com.android.calculator2*
+
+Activity Name: *com.android.calculator2.Calculator*
+
+
+# 05. Locator Strategies
+
+[Android Developer Reference](https://developer.android.com/reference/androidx/test/uiautomator/UiSelector)
+
+# 06. Writing the First Script
+
+
+# 07. 
--- src/test/java/com/tribe/workshop/appium/tests/march2024/AppiumWorkshopMarchTests.java
+++ src/test/java/com/tribe/workshop/appium/tests/march2024/AppiumWorkshopMarchTests.java
@@ -3,6 +3,7 @@ package com.tribe.workshop.appium.tests.march2024;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -12,36 +13,42 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 public class AppiumWorkshopMarchTests {
-
     @Test
-    public void myFirstTest() throws URISyntaxException, MalformedURLException, InterruptedException {
+    public void firstTest() throws URISyntaxException, MalformedURLException, InterruptedException {
         UiAutomator2Options options = new UiAutomator2Options()
                 .setPlatformName("Android")
-                .setPlatformVersion("13")
+                .setPlatformVersion("9.0")
                 .setAppPackage("com.vodqareactnative")
                 .setAppActivity("com.vodqareactnative.MainActivity")
                 .setAutomationName("UiAutomator2")
-                .setDeviceName("Mi A1");
+                .setDeviceName("Pixel 6 Pro API 28");
 
-        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
+        AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723/").toURL(), options);
+//        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
 
         Thread.sleep(5000);
-        // get username element
-        WebElement usernameElement = driver.findElement(AppiumBy.accessibilityId("username"));
-        // clear text
-        usernameElement.clear();
-        // enter text
-        usernameElement.sendKeys("admin");
-        // get password element
-        WebElement passwordElement = driver.findElement(AppiumBy.accessibilityId("password"));
-        // clear text
-        passwordElement.clear();
-        // enter text
-        passwordElement.sendKeys("admin");
 
-        // click on login button
+        // Enter username after clearing
+//        WebElement username = driver.findElement(By.xpath("//android.widget.EditText[@content-desc=\"username\"]"));
+        WebElement username = driver.findElement(AppiumBy.accessibilityId("username"));
+        username.clear();
+        Thread.sleep(2000);
+        username.sendKeys("admin");
+        Thread.sleep(2000);
+
+        // Enter password after clearing
+        WebElement password = driver.findElement(AppiumBy.accessibilityId("password"));
+        password.clear();
+        Thread.sleep(2000);
+        password.sendKeys("admin");
+        Thread.sleep(2000);
+
+        // Clicking login button
         driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"LOG IN\")")).click();
         Thread.sleep(5000);
-        driver.quit();
+
+//        driver.close(); //deprecated
+//        driver.quit();
+
     }
 }
